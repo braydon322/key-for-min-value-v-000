92d2e02bacfd96e11a9922de85fa4ccb2fe639a1
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 17:52:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,12 +7,9 @@ def key_for_min_value(name_hash)
   smallest_hash = {}
  name_hash.each do |key, value|
    if value < smallest_value
-     :smallest_key = key
-     :smallest_value = value
-     smallest_hash[:smallest_key] = :smallest_value
-
-
-
+     smallest_key = key
+     smallest_value = value
+     smallest_hash[smallest_key] = smallest_value
    end
  end
  smallest_key
