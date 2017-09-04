f31facd7d35fa20960cff752dcd344053c7bc35a
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 18:10:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,17 +5,15 @@ def key_for_min_value(name_hash)
 smallest_value = 100000
 smallest_key = ""
 smallest_hash = {}
-  name_hash.each do |key, value|
+  if name_hash = {}
+    nil
+  else
+    name_hash.each do |key, value|
      if value < smallest_value
        smallest_key = key
        smallest_value = value
      end
-  end
-
-  if key_for_min_value({}) == nil
-    nil
-  else
+    end
     smallest_key
   end
-
 end
